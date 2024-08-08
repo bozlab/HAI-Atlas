ea8c3688151660b47efbe89b0d22b5b89b521275
rishan <iamrmrishan@gmail.com>
2024-08-08 06:01:05 +0100

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,257 @@
-import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+import { siteConfig } from "@/config/site";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { MoveDownLeft, MoveUpRight, User } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+import { MyResponsiveGeoMap } from "@/components/geo-map";
+
+export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    <>
+      <section className="space-y-6 pb-6 pt-6 md:pb-8 md:pt-10 lg:py-18">
+        <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+          <Link
+            href={siteConfig.links.twitter}
+            className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium"
             target="_blank"
-            rel="noopener noreferrer"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Follow us on Twitter
+          </Link>
+          <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
+            Welcome to Health-AI Ethics Atlas
+          </h1>
+          <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+            An interactive web application designed to illustrate the application
+            and development of ethical concepts in medical AI (Artificial
+            Intelligence) across various nations.
+          </p>
+          <div className="space-x-4">
+            <Link href="/login" className={cn(buttonVariants({ size: "lg" }))}>
+              Browse
+            </Link>
+            <Link
+              href={siteConfig.links.github}
+              target="_blank"
+              rel="noreferrer"
+              className={cn(buttonVariants({ variant: "outline", size: "lg" }))}
+            >
+              Contribute
+            </Link>
+          </div>
         </div>
-      </div>
+      </section>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {/* <section
+        id="statistics"
+        className="container space-y-5 py-3 dark:bg-transparent md:py-3 lg:py-1"
+      > */}
+        {/* <div className="w-full py-10 lg:py-20"> */}
+          <div className="container" style={{ width: '100%', height: '500px' }}>
+            <MyResponsiveGeoMap/>
+            {/* <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
+              <div className="flex gap-4 flex-col items-start">
+                <div>
+                  <Badge>Platform</Badge>
+                </div>
+                <div className="flex gap-2 flex-col">
+                  <h2 className="text-xl md:text-3xl tracking-tighter lg:max-w-xl font-regular text-left">
+                    This is the start of something new
+                  </h2>
+                  <p className="text-lg lg:max-w-sm leading-relaxed tracking-tight text-muted-foreground text-left">
+                    Managing a small business today is already tough. Avoid
+                    further complications by ditching outdated, tedious trade
+                    methods. Our goal is to streamline SMB trade, making it
+                    easier and faster than ever.
+                  </p>
+                </div>
+              </div>
+              <div className="flex justify-center items-center">
+                <div className="grid text-left grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 w-full gap-2">
+                  <div className="flex bg-muted gap-0 flex-col justify-between p-6 border rounded-md">
+                    <MoveUpRight className="w-4 h-4 mb-10 text-primary" />
+                    <h2 className="text-4xl tracking-tighter max-w-xl text-left font-regular flex flex-row gap-4 items-end">
+                      500.000
+                      <span className="text-muted-foreground text-sm tracking-normal">
+                        +20.1%
+                      </span>
+                    </h2>
+                    <p className="text-base leading-relaxed tracking-tight text-muted-foreground max-w-xl text-left">
+                      Monthly active users
+                    </p>
+                  </div>
+                  <div className="flex bg-muted gap-0 flex-col justify-between p-6 border rounded-md">
+                    <MoveDownLeft className="w-4 h-4 mb-10 text-destructive" />
+                    <h2 className="text-4xl tracking-tighter max-w-xl text-left font-regular flex flex-row gap-4 items-end">
+                      20.105
+                      <span className="text-muted-foreground text-sm tracking-normal">
+                        -2%
+                      </span>
+                    </h2>
+                    <p className="text-base leading-relaxed tracking-tight text-muted-foreground max-w-xl text-left">
+                      Daily active users
+                    </p>
+                  </div>
+                  <div className="flex bg-muted gap-0 flex-col justify-between p-6 border rounded-md">
+                    <MoveUpRight className="w-4 h-4 mb-10 text-primary" />
+                    <h2 className="text-4xl tracking-tighter max-w-xl text-left font-regular flex flex-row gap-4 items-end">
+                      $523.520
+                      <span className="text-muted-foreground text-sm tracking-normal">
+                        +8%
+                      </span>
+                    </h2>
+                    <p className="text-base leading-relaxed tracking-tight text-muted-foreground max-w-xl text-left">
+                      Monthly recurring revenue
+                    </p>
+                  </div>
+                  <div className="flex bg-muted gap-0 flex-col justify-between p-6 border rounded-md">
+                    <MoveUpRight className="w-4 h-4 mb-10 text-primary" />
+                    <h2 className="text-4xl tracking-tighter max-w-xl text-left font-regular flex flex-row gap-4 items-end">
+                      $1052
+                      <span className="text-muted-foreground text-sm tracking-normal">
+                        +2%
+                      </span>
+                    </h2>
+                    <p className="text-base leading-relaxed tracking-tight text-muted-foreground max-w-xl text-left">
+                      Cost per acquisition
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div> */}
+          </div>
+        {/* </div> */}
+      {/* </section> */}
+      <section
+        id="features"
+        className="container space-y-6 bg-slate-50 py-3 dark:bg-transparent md:py-3 lg:py-1"
+      >
+        <div className="w-full py-10 lg:py-10">
+          <div className="container mx-auto">
+            <div className="flex flex-col gap-10">
+              <div className="flex gap-4 flex-col items-start">
+                <div>
+                  <Badge>Platform</Badge>
+                </div>
+                <div className="flex gap-2 flex-col">
+                  <h2 className="text-3xl md:text-5xl tracking-tighter max-w-xl font-regular text-left">
+                    Something new!
+                  </h2>
+                  <p className="text-lg max-w-xl lg:max-w-lg leading-relaxed tracking-tight text-muted-foreground  text-left">
+                    Managing a small business today is already tough.
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-col sm:grid sm:grid-cols-2 lg:grid  lg:grid-cols-3 xl:grid-cols-4 gap-8">
+                <div className="bg-muted h-full w-full rounded-md aspect-square p-6 flex justify-between flex-col lg:col-span-2 lg:row-span-2">
+                  <User className="w-8 h-8 stroke-1" />
+                  <div className="flex flex-col">
+                    <h3 className="text-xl tracking-tight">
+                      Pay supplier invoices
+                    </h3>
+                    <p className="text-muted-foreground max-w-xs text-base">
+                      Our goal is to streamline SMB trade, making it easier and
+                      faster than ever.
+                    </p>
+                  </div>
+                </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                <div className="bg-muted h-full rounded-md aspect-square p-6 flex justify-between flex-col">
+                  <User className="w-8 h-8 stroke-1" />
+                  <div className="flex flex-col">
+                    <h3 className="text-xl tracking-tight">
+                      Pay supplier invoices
+                    </h3>
+                    <p className="text-muted-foreground max-w-xs text-base">
+                      Our goal is to streamline SMB trade, making it easier and
+                      faster than ever.
+                    </p>
+                  </div>
+                </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                <div className="bg-muted h-full rounded-md aspect-square p-6 flex justify-between flex-col">
+                  <User className="w-8 h-8 stroke-1" />
+                  <div className="flex flex-col">
+                    <h3 className="text-xl tracking-tight">
+                      Pay supplier invoices
+                    </h3>
+                    <p className="text-muted-foreground max-w-xs text-base">
+                      Our goal is to streamline SMB trade, making it easier and
+                      faster than ever.
+                    </p>
+                  </div>
+                </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+                <div className="bg-muted h-full rounded-md aspect-square p-6 flex justify-between flex-col">
+                  <User className="w-8 h-8 stroke-1" />
+                  <div className="flex flex-col">
+                    <h3 className="text-xl tracking-tight">
+                      Pay supplier invoices
+                    </h3>
+                    <p className="text-muted-foreground max-w-xs text-base">
+                      Our goal is to streamline SMB trade, making it easier and
+                      faster than ever.
+                    </p>
+                  </div>
+                </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+                <div className="bg-muted h-full rounded-md aspect-square p-6 flex justify-between flex-col">
+                  <User className="w-8 h-8 stroke-1" />
+                  <div className="flex flex-col">
+                    <h3 className="text-xl tracking-tight">
+                      Pay supplier invoices
+                    </h3>
+                    <p className="text-muted-foreground max-w-xs text-base">
+                      Our goal is to streamline SMB trade, making it easier and
+                      faster than ever.
+                    </p>
+                  </div>
+                </div>
+
+                <div className="bg-muted h-full rounded-md aspect-square p-6 flex justify-between flex-col">
+                  <User className="w-8 h-8 stroke-1" />
+                  <div className="flex flex-col">
+                    <h3 className="text-xl tracking-tight">
+                      Pay supplier invoices
+                    </h3>
+                    <p className="text-muted-foreground max-w-xs text-base">
+                      Our goal is to streamline SMB trade, making it easier and
+                      faster than ever.
+                    </p>
+                  </div>
+                </div>
+
+                <div className="bg-muted h-full rounded-md aspect-square p-6 flex justify-between flex-col">
+                  <User className="w-8 h-8 stroke-1" />
+                  <div className="flex flex-col">
+                    <h3 className="text-xl tracking-tight">
+                      Pay supplier invoices
+                    </h3>
+                    <p className="text-muted-foreground max-w-xs text-base">
+                      Our goal is to streamline SMB trade, making it easier and
+                      faster than ever.
+                    </p>
+                  </div>
+                </div>
+
+                <div className="bg-muted h-full rounded-md p-6 flex justify-between flex-col lg:col-span-2">
+                  <User className="w-8 h-8 stroke-1" />
+                  <div className="flex flex-col">
+                    <h3 className="text-xl tracking-tight">
+                      Pay supplier invoices
+                    </h3>
+                    <p className="text-muted-foreground max-w-xs text-base">
+                      Our goal is to streamline SMB trade, making it easier and
+                      faster than ever.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
