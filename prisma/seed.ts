6f49a252931ff546a792ee518193f1f6be8a4cf7
rishan <iamrmrishan@gmail.com>
2024-09-24 10:36:25 +0100

    misc:updated seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,30 +1,55 @@
-// prisma/seed.ts
-
-const { PrismaClient } = require('@prisma/client');
-const fs = require('fs');
-const path = require('path');
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const countriesFilePath = path.join(__dirname, 'countries_and_ids.txt');
-  const countriesData = fs.readFileSync(countriesFilePath, 'utf-8');
-
-  const countries = countriesData.split('\n').slice(1).map((line:any) => {
-    const [name, countryCode] = line.split('\t');
-    return { name, countryCode };
+  // Find the country in data/world_countries.json. Sometimes the country name will be bit tricky to find
+  // for example, in there, there is no United Kingdom. there is only "England". use a json visualizer to read the json file.
+  const uk = await prisma.country.findUnique({
+    where: { name: 'England' },
   });
 
-  for (const country of countries) {
-    await prisma.country.updateMany({
-      where: { name: country.name }, // Match by name for existing records
-      data: {
-        countryCode: country.countryCode,
-      },
-    });
+  if (!uk) {
+    console.error('Country "England" not found in the database');
+    process.exit(1);
   }
 
-  console.log('Countries updated with countryCode!');
+  // Create a new Resource and connect it to the existing UK entry
+  const resource = await prisma.resource.create({
+    data: {
+      mainLink: 'https://www.turing.ac.uk/sites/default/files/2019-08/understanding_artificial_intelligence_ethics_and_safety.pdf',
+      include: true,
+      publicationDate: new Date('2019-01-01'), // Assuming January 1st as the publication date was not fully provided
+      organizationType: ['Research Institute', 'Private'],
+      publisher: ['Alan Turing Institute'],
+      additionalLinks: [],
+      summary:
+        "This guide provides ethical principles for AI development in the public sector, emphasizing fairness, transparency, accountability, and sustainability for responsible innovation.",
+      countries: {
+        connect: [{ id: uk.id }],
+      },
+      nlp: {
+        create: {
+          transparency: 157,
+          justiceAndFairness: 167,
+          nonMaleficence: 198,
+          responsibility: 96,
+          privacy: 15,
+          beneficence: 5,
+          freedomAndAutonomy: 12,
+          trust: 16,
+          sustainability: 18,
+          dignity: 5,
+          solidarity: 3,
+        },
+      },
+    },
+    include: {
+      countries: true, // Include related countries in the response
+    },
+  });
+
+  console.log('Resource created:', resource);
 }
 
 main()
@@ -37,6 +62,6 @@ main()
   });
 
 
-
+// use "npm run seed" to run the script. 
 
 
